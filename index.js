dca3608de7c8e7e600e1838dbedb042001845003
swkidd <kiddsoftware@gmail.com>
2021-07-05 16:33:42 +0900

    create lambda

---
+++ index.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+
+const BUCKET_NAME = process.env['BUCKET_NAME'];
+
+const s3 = new AWS.S3();
+
+exports.handler = async event => {
+  const requestContext = event.requestContext
+  try {
+    // const email = requestContext.authorizer.jwt.claims.email
+    const key = JSON.parse(event.body).key
+    let params = {
+      Bucket: BUCKET_NAME,
+      Key: key,
+      Expires: 300,
+    };
+
+    let data = await createSignedUrl(params);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data }),
+    };
+  }
+  catch (error) {
+    return {
+      statusCode: 400,
+    }
+
+  }
+};
+
+function createSignedUrl(params) {
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrl('getObject', params, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    })
+  });
+}
+    
